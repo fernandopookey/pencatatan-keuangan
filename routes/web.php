c1e9687ebce8b1641b07c1873b8db79a34d541a5
Fernando <nandopookey@gmail.com>
2024-05-15 10:58:00 +0700

    Commit pertama dari branch nando

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nando', function () {
+    return ('<h1>Branch Nando</h1>');
+});
